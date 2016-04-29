eef03b8c35a685fb59a7aacce0e653379c4a520d
Bipin Sahoo <bipinis@mindfiresolutions.com>
2016-04-29 12:20:17 +0530

    Updated files

--- src/main/java/com/mindfire/wsc/HomeController.java
+++ src/main/java/com/mindfire/wsc/HomeController.java
@@ -19,6 +19,7 @@ public class HomeController {
 	
 	/**
 	 * Simply selects the home view to render by returning its name. Its now redirect to login page
+	 * Used for Redirect
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale) {
