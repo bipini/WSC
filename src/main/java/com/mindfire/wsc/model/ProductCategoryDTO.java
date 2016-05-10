6a441890f21b9c3928d170f81d4255f8760574b2
Bipin Sahoo <bipinis@mindfiresolutions.com>
2016-05-10 16:29:18 +0530

    New DTO class for Product Category

---
+++ src/main/java/com/mindfire/wsc/model/ProductCategoryDTO.java
@@ -0,0 +1,60 @@
+/**
+ * 
+ */
+package com.mindfire.wsc.model;
+
+import javax.validation.constraints.Size;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author bipins
+ *
+ */
+
+@Service
+public class ProductCategoryDTO {	
+	
+	private int categoryId;
+	
+	@Size(min = 5, max = 100)
+	private String categoryName;
+	private String addBy;
+	/**
+	 * @return the categoryId
+	 */
+	public int getCategoryId() {
+		return categoryId;
+	}
+	/**
+	 * @param categoryId the categoryId to set
+	 */
+	public void setCategoryId(int categoryId) {
+		this.categoryId = categoryId;
+	}
+	/**
+	 * @return the categoryName
+	 */
+	public String getCategoryName() {
+		return categoryName;
+	}
+	/**
+	 * @param categoryName the categoryName to set
+	 */
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+	/**
+	 * @return the addBy
+	 */
+	public String getAddBy() {
+		return addBy;
+	}
+	/**
+	 * @param addBy the addBy to set
+	 */
+	public void setAddBy(String addBy) {
+		this.addBy = addBy;
+	}	
+	
+}
