22e5d7274c4e6aa114eade7d9355a440b0916d1b
Bipin Sahoo <bipinis@mindfiresolutions.com>
2016-05-16 16:10:41 +0530

    New methd added to get the product details, still to complete

--- src/main/java/com/mindfire/wsc/controllers/product/ProductController.java
+++ src/main/java/com/mindfire/wsc/controllers/product/ProductController.java
@@ -3,6 +3,8 @@
  */
 package com.mindfire.wsc.controllers.product;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
@@ -10,14 +12,17 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.mindfire.wsc.model.ProductCategoryDTO;
+import com.mindfire.wsc.model.ProductsDTO;
 import com.mindfire.wsc.model.UserDTO;
 import com.mindfire.wsc.service.ProductServices;
+import com.mindfire.wsc.service.UserService;
 
 /**
  * @author bipins
@@ -32,6 +37,9 @@ public class ProductController {
 	@Autowired
 	private ProductServices productServices;
 	
+	@Autowired
+	private UserService userService;	
+	
 	/**
 	 * Simply selects the home view to render by returning its name. Its now redirect to login page
 	 * Used for Redirect
@@ -39,11 +47,13 @@ public class ProductController {
 	 */
 	@RequestMapping(value = "/addcategory", method = RequestMethod.GET)
 	public String addProductCategory(HttpSession session) {
-		if(("").equals(session.getAttribute("userSession"))) {
-			logger.info("Session Not Valid");
-			return "login";
+		
+		//Check Session for Admin
+		UserDTO udto = (UserDTO)session.getAttribute("userSession");
+		if(!("admin").equals(udto.getRole())) {
+			logger.info("Admin Session Not Valid");
+			return ("redirect:/login");
 		}		
-		//logger.info("Welcome home! The client locale is {}.", locale);
 	
 		return "addcategory";
 	}
@@ -57,10 +67,14 @@ public class ProductController {
 	@ResponseBody
 	public String addProductCategoryToDB(HttpServletResponse response,
 			@PathVariable("categoryName") String categoryName, HttpSession session) {
-		if(("").equals(session.getAttribute("userSession"))) {
-			logger.info("Session Not Valid");
-			return "login";
-		}		
+		
+		//Check Session for Admin
+		UserDTO udto = (UserDTO)session.getAttribute("userSession");
+		if(!("admin").equals(udto.getRole())) {
+			logger.info("Admin Session Not Valid");
+			return ("redirect:/login");
+		}
+				
 		//logger.info("Welcome home! The client locale is {}.", locale);
 	
 		boolean flag = false;
@@ -84,14 +98,14 @@ public class ProductController {
 	 * @param ProductCategory
 	 */
 	@RequestMapping(value = "/savecategoryname", method = RequestMethod.POST)
-	public String saveProductCategory(ProductCategoryDTO categoryDTO,HttpSession session) {
-		if(("").equals(session.getAttribute("userSession"))) {
-			logger.info("Session Not Valid");
-			return "login";
-		}		
-		//logger.info("Welcome home! The client locale is {}.", locale);
-		
+	public String saveProductCategory(ProductCategoryDTO categoryDTO,HttpSession session) {			
+				
+		//Check Session for Admin
 		UserDTO user = (UserDTO)session.getAttribute("userSession");
+		if(!("admin").equals(user.getRole())) {
+			logger.info("Admin Session Not Valid");
+			return ("redirect:/login");
+		}	
 		
 		if(categoryDTO != null) {
 			
@@ -105,5 +119,36 @@ public class ProductController {
 		}
 
 		return ("redirect:/admin");		
+	}
+	
+	/*
+	 * This method is used check the user from DB
+	 * Parameter used as user id.
+	 */
+	@RequestMapping(value = "/getcategorydetail/{categoryId}", headers="Accept=*/*", method = RequestMethod.GET)
+	@ResponseBody
+	public List<ProductsDTO> getProductsUsingCategoryId(HttpServletResponse response, ModelMap model, @PathVariable("categoryId") int  categoryId,HttpSession session) {
+		List<ProductsDTO> products = null;
+		
+		//Check Session for Admin
+		UserDTO user = (UserDTO)session.getAttribute("userSession");
+		if(("admin").equals(user.getRole())) {
+			logger.info("Admin Session Not Valid");
+			//return new ModelAndView("redirect:/login");
+			
+		//Set Content type Headers
+		response.setContentType("application/json");
+	    response.setCharacterEncoding("UTF-8");
+	    
+	    //Check if Category name is not null
+		if(categoryId >= 0) {
+			products = productServices.getProductDetails(categoryId);
+		}
+		
+		//model.addAttribute("productsDetails", products);	
+		return products;
+		}
+		
+		return products;
 	}
 }
